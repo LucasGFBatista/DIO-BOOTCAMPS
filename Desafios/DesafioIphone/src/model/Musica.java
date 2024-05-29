2c80d3b07fd65b172e23dc7cf0d262068366c537
LucasGFBatista <lucas.gf.batista@gmail.com>
2024-05-29 14:11:37 -0300

    feat: added the atributes and access methods

--- Desafios/DesafioIphone/src/model/Musica.java
+++ Desafios/DesafioIphone/src/model/Musica.java
@@ -1,5 +1,37 @@
 package model;
 
 public class Musica {
+	private String nome;
+	private String nomeArtista;
+
+	// Construcotrs
+	public Musica() {
+	}
+
+	public Musica(String nome, String nomeArtista) {
+		this.nome = nome;
+		this.nomeArtista = nomeArtista;
+	}
+
+	
+	//getters adn setters
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNomeArtista() {
+		return nomeArtista;
+	}
+
+	public void setNomeArtista(String nomeArtista) {
+		this.nomeArtista = nomeArtista;
+	}
+	
+	
+	
 
 }
