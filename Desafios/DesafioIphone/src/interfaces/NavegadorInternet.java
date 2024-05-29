0cf5476d39382b0d78780ecef08d10aeff96d012
LucasGFBatista <lucas.gf.batista@gmail.com>
2024-05-29 14:10:58 -0300

    refact: change the method conexaoComInternet and deleted unused imports

--- Desafios/DesafioIphone/src/interfaces/NavegadorInternet.java
+++ Desafios/DesafioIphone/src/interfaces/NavegadorInternet.java
@@ -1,28 +1,14 @@
 package interfaces;
 
-import java.util.Random;
-
 public interface NavegadorInternet {
 
-	Random random = new Random();
-
-	private boolean conexaoComInternet() {
-		return random.nextInt(3) == 0;
-	}
-
-	default void exibirPagina() {
-		if (conexaoComInternet()) {
-			System.out.println("Exibindo pagina...");
-		} else {
-			System.out.println("Sem conexão com internet");
-		}
-	}
+	boolean conexaoComInternet();
 
 	void adicionarNovaAba();
 
 	void fecharAbaNavegador();
 
 	void atualizarPagina();
-	
+
 	void fecharPagina();
 }
