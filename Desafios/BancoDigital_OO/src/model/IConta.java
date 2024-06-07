f5a9371b3861f1f0e3cba78f14ebb8a31302a696
LucasGFBatista <lucas.gf.batista@gmail.com>
2024-06-06 22:59:15 -0300

    feat(challenge): added the implementation of the TransacaoValorInvalidoException

--- Desafios/BancoDigital_OO/src/model/IConta.java
+++ Desafios/BancoDigital_OO/src/model/IConta.java
@@ -1,9 +1,12 @@
 package model;
 
+import model.exception.TransacaoValorInvalidoException;
+
 public interface IConta {
 
-	void sacar(double valor);
-	void depositar(double valor);
-	void transferir(double valor, IConta contaDestino);
+	void sacar (double valor) throws TransacaoValorInvalidoException;
+	void depositar(double valor) throws TransacaoValorInvalidoException;
+	void transferir(double valor, IConta contaDestino) throws TransacaoValorInvalidoException;
 	void imprimirExtrato();
+	
 }
