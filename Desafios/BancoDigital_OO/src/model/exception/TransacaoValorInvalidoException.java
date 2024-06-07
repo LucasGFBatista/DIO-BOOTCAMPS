d87dd1a54a64b47270e2316aa887193113769928
LucasGFBatista <lucas.gf.batista@gmail.com>
2024-06-06 22:58:00 -0300

    feat(challenge): created TransacaoValorInvalidoException

---
+++ Desafios/BancoDigital_OO/src/model/exception/TransacaoValorInvalidoException.java
@@ -0,0 +1,8 @@
+package model.exception;
+
+public class TransacaoValorInvalidoException extends Exception {
+
+    public TransacaoValorInvalidoException(String mensagem) {
+        super(mensagem);
+    }
+}
