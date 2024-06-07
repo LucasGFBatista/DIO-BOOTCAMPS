b305a53345732b62b1e2d7d7ce96ed0327600d72
LucasGFBatista <lucas.gf.batista@gmail.com>
2024-06-07 11:17:24 -0300

    fix(challenge): Change the type in parameter transferencia

--- Desafios/BancoDigital_OO/src/model/IConta.java
+++ Desafios/BancoDigital_OO/src/model/IConta.java
@@ -6,7 +6,7 @@ public interface IConta {
 
 	void sacar (double valor) throws TransacaoValorInvalidoException;
 	void depositar(double valor) throws TransacaoValorInvalidoException;
-	void transferir(double valor, IConta contaDestino) throws TransacaoValorInvalidoException;
+	void transferir(double valor, Conta contaDestino) throws TransacaoValorInvalidoException;
 	void imprimirExtrato();
 	
 }
