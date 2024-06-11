8edc82db0d74d8c3899235c406dd25d6da24ae20
LucasGFBatista <lucas.gf.batista@gmail.com>
2024-06-11 09:45:39 -0300

    feat(challenge): Solved the RegistroTransacoesComStream challenge

--- Desafios/poo-java-prativa/src/registroTransacaoStreamAPI/RegistroTransacoesComStream.java
+++ Desafios/poo-java-prativa/src/registroTransacaoStreamAPI/RegistroTransacoesComStream.java
@@ -7,56 +7,64 @@ import java.util.stream.Collectors;
 
 public class RegistroTransacoesComStream {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
-        double saldo = scanner.nextDouble();
-        int quantidadeTransacoes = scanner.nextInt();
+		double saldo = scanner.nextDouble();
+		int quantidadeTransacoes = scanner.nextInt();
 
-        List<Transacao> transacoes = new ArrayList<>();
+		List<Transacao> transacoes = new ArrayList<>();
 
-        for (int i = 1; i <= quantidadeTransacoes; i++) {
-          
-            char tipoTransacao = scanner.next().charAt(0);
-            double valorTransacao = scanner.nextDouble();
+		for (int i = 1; i <= quantidadeTransacoes; i++) {
 
-            // TODO: Criar uma nova transação e adicioná-la à lista de transações
-            Transacao transacao = null;
+			char tipoTransacao = scanner.next().charAt(0);
+			double valorTransacao = scanner.nextDouble();
 
-            // Verifica e atualiza o saldo da conta com base no tipo de transação
-            if (transacao.getTipo().toUpperCase() == 'D') {
-                saldo += valorTransacao;
-            } else if (transacao.getTipo().toUpperCase() == 'S') {
-                saldo -= valorTransacao;
-            }
-        }
+			// TODO: Criar uma nova transação e adicioná-la à lista de transações
+			Transacao transacao = new Transacao(tipoTransacao, valorTransacao);
 
-        System.out.println("\nSaldo : " + saldo);
-        System.out.println("\nTransacoes:");
-        transacoes.stream()
-                .map(transacao -> "TODO: Formatar a Saída (tipo e valor) de acordo com os Exemplos.")
-                .collect(Collectors.toList())
-                .forEach(System.out::println);
+			transacoes.add(transacao);
 
-        // Fechar o scanner para evitar vazamentos de recursos
-        scanner.close();
-    }
+			// Verifica e atualiza o saldo da conta com base no tipo de transação
+			if (transacao.getTipo() == 'D' || transacao.getTipo() == 'd') {
+				saldo += valorTransacao;
+
+			} else if (transacao.getTipo() == 'S' || transacao.getTipo() == 's') {
+				saldo -= valorTransacao;
+			}
+		}
+
+		System.out.println("Saldo : " + saldo);
+		System.out.println("Transacoes:");
+		transacoes.stream()
+				.filter(transacao -> 
+						   transacao.getTipo() == 'D' 
+						|| transacao.getTipo() == 'd'
+						|| transacao.getTipo() == 'S' 
+						|| transacao.getTipo() == 's')
+				.map(t -> t.getTipo() + " de " + t.getValor())
+				.collect(Collectors.toList())
+				.forEach(System.out::println);
+
+		// Fechar o scanner para evitar vazamentos de recursos
+		scanner.close();
+	}
 }
 
 class Transacao {
-    private char tipo;
-    private double valor;
+	private char tipo;
+	private double valor;
 
-    public Transacao(char tipo, double valor) {
-        this.tipo = tipo;
-        this.valor = valor;
-    }
+	public Transacao(char tipo, double valor) {
+		this.tipo = tipo;
+		this.valor = valor;
+	}
 
-    public char getTipo() {
-        return tipo;
-    }
+	public char getTipo() {
+		return tipo;
+	}
 
-    public double getValor() {
-        return valor;
-    }
+	public double getValor() {
+		return valor;
+	}
 }
