3814a38d841b097c6393f33ba853f91ef0262510
LucasGFBatista <lucas.gf.batista@gmail.com>
2024-07-15 13:20:52 -0300

    feat(practice): Aula sobre Spring JPA

---
+++ PraticandoDuranteAula/Spring-boot/Aula-spring-data-jpa/src/test/java/com/lucas/Aula_spring_data_jpa/AulaSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.Aula_spring_data_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaSpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
