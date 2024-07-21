1cdcb72da1ba67d411f70024c91e607c073cfba2
LucasGFBatista <lucas.gf.batista@gmail.com>
2024-07-21 00:15:16 -0300

    feat(practice): Start Desigin Pattern with Spring Boot

---
+++ PraticandoDuranteAula/design-pattern/design-pattern-spring/src/main/java/com/lucasgfbatista/gof/Application.java
@@ -0,0 +1,28 @@
+package com.lucasgfbatista.gof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/*
+ * Projeto Spring Boot gerado via Spring Initializr
+ * Os Seguintes módulo foram selecionados:
+ * - Spring Data JPA
+ * - Spring Web
+ * - H2 Database
+ * - OpenFeign
+ *
+ * @author lucasgfbatista
+ *
+ * */
+
+
+@EnableFeignClients
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
