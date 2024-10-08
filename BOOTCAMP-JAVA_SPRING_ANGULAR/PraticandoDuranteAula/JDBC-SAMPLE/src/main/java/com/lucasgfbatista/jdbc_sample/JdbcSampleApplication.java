8a9359938259e5e03442b76c36dea23f536d49d5
LucasGFBatista <lucas.gf.batista@gmail.com>
2024-10-08 12:59:42 -0300

    feat(practice): JDBC

---
+++ BOOTCAMP-JAVA_SPRING_ANGULAR/PraticandoDuranteAula/JDBC-SAMPLE/src/main/java/com/lucasgfbatista/jdbc_sample/JdbcSampleApplication.java
@@ -0,0 +1,13 @@
+package com.lucasgfbatista.jdbc_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcSampleApplication.class, args);
+	}
+
+}
